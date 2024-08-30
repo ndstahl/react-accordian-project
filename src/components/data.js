f35097a844cc00f730ba17aa891636b4c7ceffaf
Nick Stahl <nickstahl@Clintons-MBP-2.lan>
2024-08-29 22:45:27 -0400

    Added Random Color Section to Project

---
+++ src/components/data.js
@@ -0,0 +1,28 @@
+const data = [
+	{
+	    id : "1",
+	    question: 'What are accordion components?',
+	    answer:
+	       'Accordian components are user inferface elements used for organ…'
+	},
+	{
+	    id : "2",
+	    question: 'What are they used for?',
+	    answer:
+	       'They are commonly user for various contexts, including FAQs…'
+	},
+	{
+	    id : "3",
+	    question: 'Accordians are a musical instrument',
+	    answer:
+	       'Accordian as a musical instrument has a set of keys and a bellow to make ….'
+	},
+	{
+	    id : "4",
+	    question: 'Can I create an accordian component with a different framework?',
+	    answer:
+	       'Absolutely!'
+	},
+];
+
+export default data
