5c80181f5110b7bc544c354f19505e15a36cf6bd
Nick Stahl <nickstahl@Clintons-MBP-2.lan>
2024-08-30 00:48:22 -0400

    Added Star Rating Component to Project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
 
       {/* Random color component */}
       <RandomColor/>
+
+      {/* Star Rating Component */}
+      <StarRating noOfStars={10}/>
     </div>
   );
 }
